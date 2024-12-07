23b628d56c1cf45ea21dcb1f8c226cd1a2246e67
tahmeed shahriyar <tahmeedshahriyar312@gmail.com>
2024-12-07 09:21:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  // <script src="https://cdn.tailwindcss.com"></script>
